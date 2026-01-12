2694a4fbf54591b88bc5a825280cb35aa6bf8257
VegetalDev <chemky2000@gmail.com>
2026-01-11 19:33:37 -0800

    build: fatjar runtime dependencies of main subproject

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -1,7 +1,18 @@
 import org.echoesfrombeyond.gradle.plugin.JavaConventionPlugin
+import org.gradle.jvm.tasks.Jar
 
 apply<JavaConventionPlugin>()
 
 dependencies {
     add("implementation", project(":util"))
+}
+
+tasks.named<Jar>("jar").configure {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // Packages all (runtime) dependencies in the jar file, including the compiled output of project
+    // dependencies.
+    from(configurations.named<Configuration>("runtimeClasspath").map { configuration ->
+        configuration.map { file -> if (file.isDirectory) file else zipTree(file) }
+    })
 }
