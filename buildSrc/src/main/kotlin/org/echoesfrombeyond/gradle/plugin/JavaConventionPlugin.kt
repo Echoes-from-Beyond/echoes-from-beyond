28dcdcfacb33dc805cce82d8cf4cb02e9dc994b1
VegetalDev <chemky2000@gmail.com>
2026-03-12 22:27:57 -0700

    build: decompilation task using Vineflower
    Also compile against the local Hytale version to ensure no mismatch during development. Will change this back once/if source code is made available and proper versioning is a thing.

--- buildSrc/src/main/kotlin/org/echoesfrombeyond/gradle/plugin/JavaConventionPlugin.kt
+++ buildSrc/src/main/kotlin/org/echoesfrombeyond/gradle/plugin/JavaConventionPlugin.kt
@@ -1,7 +1,8 @@
 package org.echoesfrombeyond.gradle.plugin
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import java.net.URI
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.jvm.java
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
@@ -124,19 +125,21 @@ fun Project.withHytaleDependency() {
   if (plugins.withType(JavaConventionPlugin::class.java).isEmpty())
       throw GradleException("Hytale plugin projects must apply JavaConventionPlugin!")
 
-  repositories.exclusiveContent { exclusive ->
-    exclusive.forRepository {
-      repositories.maven { maven ->
-        maven.name = "hytale-pre-release"
-        maven.url = URI.create("https://maven.hytale.com/pre-release")
-      }
-    }
-    exclusive.filter { filter -> filter.includeGroup("com.hypixel.hytale") }
+  val hytaleJar = provider {
+    val dotfile = rootDir.resolve(".hytale")
+    if (!dotfile.exists()) throw GradleException("Missing .hytale file!")
+
+    val contents = dotfile.readText(Charsets.UTF_8).trim()
+    val hytaleServer = Path.of(contents).resolve("Server").resolve("HytaleServer.jar")
+
+    if (!Files.exists(hytaleServer))
+        throw GradleException("Path specified in .hytale does not point to a valid installation!")
+
+    hytaleServer
   }
 
-  val hytale = "com.hypixel.hytale:Server:latest.integration"
-  dependencies.add("compileOnly", hytale)
-  dependencies.add("testImplementation", hytale)
+  dependencies.add("compileOnly", files(hytaleJar))
+  dependencies.add("testImplementation", files(hytaleJar))
 }
 
 /**
