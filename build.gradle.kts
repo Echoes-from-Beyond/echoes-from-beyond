1e0e3642ddea0ec19513492a0bc108652c536eed
Kyle <chemky2000@gmail.com>
2026-03-31 06:16:12 +0000

    ci: include plugin version validation checks
    * build: new tasks for validating plugin versioning
    
    * fix: wrong error message
    
    * ci: switch to release patchline
    
    * chore: simplify build script
    
    * chore: slightly more idiomatic usage
    
    * chore: bump server versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 import com.diffplug.spotless.LineEnding
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonArray
+import com.google.gson.JsonNull
+import com.google.gson.JsonObject
+import java.io.BufferedInputStream
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
+import java.util.jar.Manifest
 import org.echoesfrombeyond.gradle.plugin.HytaleDecompiler
-import org.gradle.internal.extensions.core.extra
 
 plugins { id("com.diffplug.spotless") version "8.3.0" }
 
@@ -44,17 +53,216 @@ val copySdkTask: TaskProvider<Copy> =
       from(serverJar, serverAot, assetsZip).into(runDirectory)
     }
 
+val pluginJarFiles: Provider<List<Provider<File>>> = provider {
+  subprojects
+      .filter { sub -> sub.extra.has("hasPlugin") }
+      .filter { sub -> sub.extra.get("hasPlugin") as? Boolean ?: false }
+      .map { sub ->
+        sub.tasks.named("shadowJar").map { shadowJarTask -> shadowJarTask.outputs.files.singleFile }
+      }
+}
+
+val pluginZipTrees: Provider<List<Provider<FileCollection>>> =
+    pluginJarFiles.map { providerList ->
+      providerList.flatMap { provider ->
+        listOf(
+            provider.map { file -> files(file) },
+            provider.map { file ->
+              zipTree(file).matching {
+                include("META-INF/MANIFEST.MF")
+                include("manifest.json")
+              }
+            },
+        )
+      }
+    }
+
+val versionReport: TaskProvider<DefaultTask> =
+    tasks.register("versionReport", DefaultTask::class.java) {
+      inputs.files(pluginZipTrees)
+      outputs.file(layout.buildDirectory.file("versionReport.json"))
+
+      doLast {
+        val gson = GsonBuilder().disableHtmlEscaping().serializeNulls().create()
+
+        val reports = JsonArray()
+        val iterator = inputs.files.iterator()
+
+        var save: File? = null
+        while (iterator.hasNext() || save != null) {
+          val jarFile =
+              if (save == null) {
+                iterator.next()
+              } else {
+                val copy = save
+                save = null
+                copy
+              }
+
+          val lookahead = mutableMapOf<String, File>()
+          while (iterator.hasNext()) {
+            val next = iterator.next()
+            if (next.extension == "jar") {
+              save = next
+              break
+            } else {
+              lookahead[next.extension] = next
+            }
+          }
+
+          val manifestFile = lookahead["MF"]
+          val jsonFile = lookahead["json"]
+
+          val report = JsonObject()
+
+          report.addProperty("FileName", jarFile.name)
+          report.add("JsonVersion", JsonNull.INSTANCE)
+          report.add("ManifestVersion", JsonNull.INSTANCE)
+          report.add("HytaleVersion", JsonNull.INSTANCE)
+
+          if (manifestFile != null) {
+            BufferedInputStream(
+                    Files.newInputStream(manifestFile.toPath(), StandardOpenOption.READ)
+                )
+                .use { inputStream ->
+                  val manifest = Manifest(inputStream)
+
+                  val attributes = manifest.mainAttributes
+
+                  val implementationVersion = attributes.getValue("Implementation-Version")
+                  val hytaleVersion = attributes.getValue("Hytale-Version")
+
+                  report.addProperty("ManifestVersion", implementationVersion)
+                  report.addProperty("HytaleVersion", hytaleVersion)
+                }
+          }
+
+          if (jsonFile != null) {
+            Files.newBufferedReader(jsonFile.toPath(), StandardCharsets.UTF_8).use { reader ->
+              report.addProperty(
+                  "JsonVersion",
+                  gson.fromJson(reader, JsonObject::class.java).get("Version")?.asString,
+              )
+            }
+          }
+
+          reports.add(report)
+        }
+
+        val sortedReports = JsonArray(reports.size())
+
+        reports
+            .sortedBy { element -> element.asJsonObject["FileName"].asString }
+            .forEach { sorted -> sortedReports.add(sorted) }
+
+        gson
+            .newJsonWriter(
+                Files.newBufferedWriter(
+                    outputs.files.singleFile.toPath(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING,
+                )
+            )
+            .use { jsonWriter -> gson.toJson(sortedReports, jsonWriter) }
+      }
+    }
+
+class Util {
+  companion object {
+    fun maybeThrowErrors(errors: MutableList<Pair<String, String>>) {
+      if (!errors.isEmpty())
+          throw GradleException(
+              errors.joinToString("\n") { pair -> "[${pair.first}] ${pair.second}" }
+          )
+    }
+
+    fun checkVersion(
+        report: JsonObject,
+        errors: MutableList<Pair<String, String>>,
+        clientHytaleVersion: String?,
+    ) {
+      val fileName = report.get("FileName").asString
+      val jsonVersion = report.get("JsonVersion")
+      val manifestVersion = report.get("ManifestVersion").asString
+      val hytaleVersion = report.get("HytaleVersion").asString
+
+      if (jsonVersion.isJsonNull) {
+        errors.add(Pair(fileName, "`manifest.json` is missing the Version field"))
+        return
+      }
+
+      if (jsonVersion.asString != manifestVersion) {
+        errors.add(
+            Pair(
+                fileName,
+                "Plugin version mismatch: `manifest.json` specifies ${jsonVersion.asString} while Gradle specifies $manifestVersion",
+            )
+        )
+        return
+      }
+
+      if (clientHytaleVersion != null && clientHytaleVersion != hytaleVersion) {
+        errors.add(
+            Pair(
+                fileName,
+                "Hytale version mismatch: plugin was built for $hytaleVersion but client is on $clientHytaleVersion",
+            )
+        )
+      }
+    }
+  }
+}
+
+val validateVersions: TaskProvider<DefaultTask> =
+    tasks.register("validateVersions", DefaultTask::class.java) {
+      group = "verification"
+      inputs.file(
+          serverJar.map { jar ->
+            zipTree(jar).matching { include("META-INF/MANIFEST.MF") }.singleFile
+          }
+      )
+      inputs.file(versionReport.map { task -> task.outputs.files.singleFile })
+      outputs.files()
+
+      doLast {
+        val gson = Gson()
+
+        val fileMap = mutableMapOf<String, File>()
+        inputs.files.forEach { file -> fileMap[file.extension] = file }
+
+        val hytaleManifest = fileMap["MF"]
+        val versionReport = fileMap["json"]
+
+        if (hytaleManifest == null || versionReport == null)
+            throw GradleException("Internal error: missing Hytale manifest or version report file")
+
+        val hytaleImplementationVersion: String?
+        BufferedInputStream(Files.newInputStream(hytaleManifest.toPath(), StandardOpenOption.READ))
+            .use { inputStream ->
+              hytaleImplementationVersion =
+                  Manifest(inputStream).mainAttributes.getValue("Implementation-Version")
+            }
+
+        if (hytaleImplementationVersion == null)
+            throw GradleException("Unable to determine the Hytale client version")
+
+        val errorMessages = mutableListOf<Pair<String, String>>()
+        Files.newBufferedReader(versionReport.toPath(), StandardCharsets.UTF_8)
+            .use { reader -> gson.fromJson(reader, JsonArray::class.java) }
+            .map { element -> element.asJsonObject }
+            .forEach { report ->
+              Util.checkVersion(report, errorMessages, hytaleImplementationVersion)
+            }
+
+        Util.maybeThrowErrors(errorMessages)
+      }
+    }
+
 val syncPluginsTask: TaskProvider<Sync> =
     tasks.register("syncPlugins", Sync::class.java) {
       // Copy from all subprojects that have the `hasPlugin` property set to `true`. This is only
       // the case when their build script includes `withHytalePlugin`.
-      from(
-              subprojects
-                  .filter { sub -> sub.extra.has("hasPlugin") }
-                  .filter { sub -> sub.extra.get("hasPlugin") as? Boolean ?: false }
-                  .map { sub -> sub.tasks.named("shadowJar") }
-          )
-          .into(runDirectory.dir("mods"))
+      from(pluginJarFiles).into(runDirectory.dir("mods"))
 
       // Preserve everything except run/mods/*.jar.
       preserve {
@@ -64,7 +272,7 @@ val syncPluginsTask: TaskProvider<Sync> =
     }
 
 tasks.register("runDevServer", JavaExec::class.java) {
-  inputs.files(copySdkTask, syncPluginsTask)
+  inputs.files(copySdkTask, syncPluginsTask, validateVersions)
 
   // Pass through commands to the Hytale server.
   standardInput = System.`in`
