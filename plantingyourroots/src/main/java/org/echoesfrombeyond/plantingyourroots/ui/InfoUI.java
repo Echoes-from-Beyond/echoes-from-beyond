8f71c072c4930784d9743bf5884247e8c48eb5eb
VegetalDev <chemky2000@gmail.com>
2026-04-27 19:08:36 -0700

    fix: call sendUpdate() in handleDataEvent

--- plantingyourroots/src/main/java/org/echoesfrombeyond/plantingyourroots/ui/InfoUI.java
+++ plantingyourroots/src/main/java/org/echoesfrombeyond/plantingyourroots/ui/InfoUI.java
@@ -69,5 +69,6 @@ public class InfoUI extends InteractiveCustomUIPage<InfoUI.Data> {
   public void handleDataEvent(Ref<EntityStore> ref, Store<EntityStore> store, InfoUI.Data data) {
     var player = store.getComponent(ref, Player.getComponentType());
     if (player != null) player.getPageManager().setPage(ref, store, Page.None);
+    sendUpdate();
   }
 }
